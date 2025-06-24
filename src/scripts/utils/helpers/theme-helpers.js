4025e6f66d991d38e94c624ff0a420675019522f
FabianGenell <fabian.genell@hyperisland.se>
2025-06-24 09:38:13 +0200

    Fixed external links

--- src/scripts/utils/helpers/theme-helpers.js
+++ src/scripts/utils/helpers/theme-helpers.js
@@ -1,15 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.addEventListener('click', (evt) => {
         const link = evt.target.tagName === 'A' ? evt.target : evt.target.closest('a');
-        if (link && link.tagName === 'A' && window.location.hostname !== new URL(link.href).hostname) {
+        setExternalLink(link);
+    });
+
+    document.querySelectorAll('a').forEach((link) => {
+        setExternalLink(link);
+    });
+
+    function setExternalLink(link) {
+        if (
+            link &&
+            link.tagName === 'A' &&
+            link.href &&
+            window.location.hostname !== new URL(link.href).hostname
+        ) {
             link.target = '_blank';
         }
-    });
+    }
 
     // Ensure anchor scrolling is smooth (this shouldn't be added in the CSS)
     document.addEventListener('click', (evt) => {
         if (
             evt.target.tagName === 'A' &&
+            evt.target.href &&
             window.location.hostname === new URL(evt.target.href).hostname &&
             evt.target.href.includes('#')
         ) {
